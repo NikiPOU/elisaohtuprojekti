815f03c19564e6c367ba83fc92e9f5802e4a5d54
Sonja Silvonen <sonja.silvonen@outlook.com>
2024-09-16 13:20:52 +0300

    Add basic movement script

---
+++ Assets/Scripts/Terrorist1Movement.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Terrorist1Movement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += new Vector3(0, 0, 0.001f);
+        
+    }
+}
