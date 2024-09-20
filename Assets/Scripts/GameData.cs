e7ca0178e35dcf4f6f5bd2b6b022f11f6dacab8d
Maija <maija.rislakki@helsinki.fi>
2024-09-20 14:49:30 +0300

    render text on the screen from scriptt

---
+++ Assets/Scripts/GameData.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+
+public class Statistics : MonoBehaviour
+{
+
+    public TMP_Text text;
+
+    void Start()
+    {
+        if (text != null)
+        {
+
+            text.text = "Game statistics here";
+            Debug.Log("Teksti on päivitetty"); // Log success message
+        }
+        else
+        {
+            Debug.LogError("Ei toimi: Teksti-komponentti puuttuu.");
+        }
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
