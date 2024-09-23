bebb8c692ac3ce55609d0b4db47d006b50c26900
Savonius <essi.savonius@hotmail.fi>
2024-09-23 12:34:44 +0300

    Add GSI data listener app.py

---
+++ gsi/app.py
@@ -0,0 +1,18 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+latest_data = {}
+
+@app.route('/', methods=['POST'])
+def index():
+    global latest_data
+    latest_data = request.get_json()
+    return jsonify(status="success"), 200
+
+@app.route('/data', methods=['GET'])
+def get_data():
+    return jsonify(latest_data)
+
+if __name__ == '__main__':
+    app.run(host='localhost', port=3000)
