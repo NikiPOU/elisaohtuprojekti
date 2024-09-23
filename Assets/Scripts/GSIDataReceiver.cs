9be4cf4d272f37b60d0e5f851f0788bb58b617a9
Savonius <essi.savonius@hotmail.fi>
2024-09-23 12:31:16 +0300

    Add GSI data receiver script

---
+++ Assets/Scripts/GSIDataReceiver.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class GSIDataReceiver : MonoBehaviour
+{
+    private void Start()
+    {
+        StartCoroutine(GetGSIData("http://localhost:3000/data"));   // Kovakoodattuna portti 3000 väliaikaisesti
+    }
+
+    private IEnumerator GetGSIData(string uri)
+    {
+        while (true)
+        {
+            using (UnityWebRequest webRequest = UnityWebRequest.Get(uri))
+            {
+                yield return webRequest.SendWebRequest();
+
+                // Jos Error -> printtaa sen Unity terminaaliin
+                if (webRequest.result == UnityWebRequest.Result.ConnectionError || 
+                    webRequest.result == UnityWebRequest.Result.ProtocolError)
+                {
+                    Debug.Log("Error: " + webRequest.error);
+                }
+                else
+                {
+                    // Printtaa GSI Datan Unity terminaaliin
+                    Debug.Log("GSI Data: " + webRequest.downloadHandler.text);
+                }
+            }
+
+            yield return new WaitForSeconds(0.1f); // Odotus
+        }
+    }
+}
