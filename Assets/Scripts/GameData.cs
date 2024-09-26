a58b45284e83ce1a82e457861e62417e30082163
Savonius <essi.savonius@hotmail.fi>
2024-09-26 19:16:02 +0300

    Add packages

--- Assets/Scripts/GameData.cs
+++ Assets/Scripts/GameData.cs
@@ -2,30 +2,49 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using Newtonsoft.Json.Linq;
 
 
 public class Statistics : MonoBehaviour
 {
 
-    public TMP_Text text;
-
+    public TMP_Text text; //textMeshPro komponentti!
+    public GSIDataReceiver gsiDataReceiver; //GSIDataReceiver scriptistä komponentti
     void Start()
     {
-        if (text != null)
+        if (text == null)
         {
-
-            text.text = "Game statistics here";
-            Debug.Log("Teksti on päivitetty"); // Log success message
+            Debug.Log("Teksti komponentissa ongelma"); 
         }
-        else
+        
+        gsiDataReceiver = FindObjectOfType<GSIDataReceiver>();
+        if (gsiDataReceiver == null)
         {
-            Debug.LogError("Ei toimi: Teksti-komponentti puuttuu.");
+            Debug.LogError("Ei toimi: GSI datan haussa ongelma.");
         }
     }
-
 
     void Update()
     {
-        
+        if (gsiDataReceiver != null && text != null)
+        {
+            //tässä eritellään tärkeä data turhasta
+            string important = ParseGSI(gsiDataReceiver.gsiData);
+
+            text.text = important; //päivittää serveriltä saatua dataa näytölle
+        }
+
     }
+    string ParseGSI(string jsonData)
+    {
+
+        JObject data = JObject.Parse(jsonData); //parse newtonsoftilla
+        string playerName = data["player"]?["name"]?.ToString();
+        string handledData = $"Player: {playerName}\n";
+
+        return handledData;
+    }
+
+
+
 }
