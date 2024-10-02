004584eadc1c91aa770f5735b45eb93b4a07de56
Miikka <miikka.seppi123@gmail.com>
2024-10-02 15:29:20 +0300

    Make playmodetests for gamemenu

--- Assets/Scripts/GameMenuManager.cs
+++ Assets/Scripts/GameMenuManager.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -9,22 +7,32 @@ public class GameMenuManager : MonoBehaviour
     public float spawnDistance = 2;
     public GameObject menu;
     public InputActionProperty showButton;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
     void Update()
     {
-        if(showButton.action.WasPressedThisFrame())
+        // Only proceed if showButton action is set and enabled
+        if (showButton.action != null && showButton.action.WasPressedThisFrame())
         {
-            menu.SetActive(!menu.activeSelf);
-
-            menu.transform.position = head.position + new Vector3(head.forward.x, 0, head.forward.z) * spawnDistance;
+            ToggleMenu();
         }
-        
+
+        // Ensure the menu position is updated only when it's active
+        if (menu.activeSelf)
+        {
+            UpdateMenuPosition();
+        }
+    }
+
+    public void ToggleMenu()
+    {
+        // Toggle the menu's active state
+        menu.SetActive(!menu.activeSelf);
+    }
+
+    public void UpdateMenuPosition()
+    {
+        // Update the menu's position based on the head's position
+        menu.transform.position = head.position + new Vector3(head.forward.x, 0, head.forward.z) * spawnDistance;
         menu.transform.LookAt(new Vector3(head.position.x, menu.transform.position.y, head.position.z));
         menu.transform.forward *= -1;
     }
