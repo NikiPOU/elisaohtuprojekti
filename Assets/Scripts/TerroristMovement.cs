fcee444dbf169f7ab8c2276437b662ae6b22fca2
Sonja Silvonen <sonja.silvonen@outlook.com>
2024-10-09 16:13:01 +0300

    Change z-coordinate

--- Assets/Scripts/TerroristMovement.cs
+++ Assets/Scripts/TerroristMovement.cs
@@ -71,7 +71,7 @@ public class TerroristMovement : MonoBehaviour
                 float x_coord = float.Parse(coords[0]);
                 float y_coord = float.Parse(coords[1]);
                 float z_coord = float.Parse(coords[2]);
-                Vector3 position = new Vector3(0.001f * x_coord, 0.501f, 0.001f * z_coord);
+                Vector3 position = new Vector3(0.001f * x_coord, 0.501f, 1 + 0.001f * z_coord);
                 terrorists.Add(playerName, position);
             }
             
