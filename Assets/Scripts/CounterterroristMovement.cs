c991da31963eb5b596f2ebf62a99e615f32a0375
Sonja Silvonen <sonja.silvonen@outlook.com>
2024-10-09 16:10:47 +0300

    Change y-coordinate

--- Assets/Scripts/CounterterroristMovement.cs
+++ Assets/Scripts/CounterterroristMovement.cs
@@ -72,7 +72,7 @@ public class CounterterroristMovement : MonoBehaviour
                 float x_coord = float.Parse(coords[0]);
                 float y_coord = float.Parse(coords[1]);
                 float z_coord = float.Parse(coords[2]);
-                Vector3 position = new Vector3(0.001f * x_coord, 0, 0.001f * z_coord);
+                Vector3 position = new Vector3(0.001f * x_coord, 0.501f, 0.001f * z_coord);
                 cterrorists.Add(playerName, position);
             }
             
