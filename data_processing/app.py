09ed2a67f35efdc058ced0e618dcd3804be383a7
mseppi <miikka.seppi123@gmail.com>
2024-11-06 19:03:23 +0200

    Make program to connect to database

--- data_processing/app.py
+++ data_processing/app.py
@@ -1,10 +1,11 @@
 from flask import Flask, request, jsonify
 from gsi_data_receiver import DataReceiver
+from db import create_database
 
 app = Flask(__name__)
 data_receiver = DataReceiver()
 
-@app.route('/', methods=['POST']) #Get JSON data from request
+@app.route('/', methods=['POST'])
 def receive_data():
     try:
         latest_data = request.get_json()
@@ -15,4 +16,5 @@ def receive_data():
         return jsonify(status="error", error=str(e)), 500
 
 if __name__ == '__main__':
+    create_database()
     app.run(host='0.0.0.0', port=3000)
--- data_processing/db.py
+++ data_processing/db.py
@@ -1,20 +1,21 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
 from os import environ
 
+def create_database():
+    DATABASE_URL = environ.get("DATABASE_URL")
+    DATABASE_USERNAME = environ.get("DATABASE_USERNAME")
+    DATABASE_PASSWORD = environ.get("DATABASE_PASSWORD")
+    DATABASE_PORT = environ.get("DATABASE_PORT")
 
-DATABASE_URL = environ.get("DATABASE_URL")
-DATABASE_USERNAME = environ.get("DATABASE_USERNAME")
-DATABASE_PASSWORD = environ.get("DATABASE_PASSWORD")
-DATABASE_PORT = environ.get("DATABASE_PORT")
+    database_uri = f"postgresql://{DATABASE_USERNAME}:{DATABASE_PASSWORD}@{DATABASE_URL}:{DATABASE_PORT}"
 
-database_uri = f"postgresql://{DATABASE_USERNAME}:{DATABASE_PASSWORD}{DATABASE_URL}:{DATABASE_PORT}"
+    engine = create_engine(database_uri)
 
-engine = create_engine(database_uri)
-
-try: 
-    with engine.connect() as connection:
-        result = connection.execute("SELECT version();")
-        print(result.fetchone())
-        print("Connected to PostgreSQL, version[0]")
-except Exception as e:
-    print(e)
+    try: 
+        with engine.connect() as connection:
+            sql = text("SELECT version();")
+            result = connection.execute(sql)
+            version = result.fetchone()
+            print(f"Connected to PostgreSQL, version: {version[0]}")
+    except Exception as e:
+        print(e)
