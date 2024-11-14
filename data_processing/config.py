215d709ccaab564ea7d96c60739ff083c79ac993
mseppi <miikka.seppi123@gmail.com>
2024-11-14 19:44:10 +0200

    make database working in openshift, add schema initializing file

---
+++ data_processing/config.py
@@ -0,0 +1,8 @@
+from os import environ
+
+DATABASE_URL = environ.get("DATABASE_URL")
+DATABASE_USERNAME = environ.get("DATABASE_USERNAME")
+DATABASE_PASSWORD = environ.get("DATABASE_PASSWORD")
+DATABASE_PORT = environ.get("DATABASE_PORT")
+
+database_uri = f"postgresql://{DATABASE_USERNAME}:{DATABASE_PASSWORD}@{DATABASE_URL}:{DATABASE_PORT}"
