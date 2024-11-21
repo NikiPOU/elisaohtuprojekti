3c65284bb77d1866a9c9286c7e6812b2d43a8fc6
mseppi <miikka.seppi123@gmail.com>
2024-11-21 18:57:04 +0200

    fix correct database

--- data_processing/config.py
+++ data_processing/config.py
@@ -5,5 +5,4 @@ DATABASE_USERNAME = environ.get("DATABASE_USERNAME")
 DATABASE_PASSWORD = environ.get("DATABASE_PASSWORD")
 DATABASE_PORT = environ.get("DATABASE_PORT")
 
-#database_uri = f"postgresql://{DATABASE_USERNAME}:{DATABASE_PASSWORD}@{DATABASE_URL}:{DATABASE_PORT}"
-database_uri = "postgresql+psycopg2:///mseppi"
+database_uri = f"postgresql://{DATABASE_USERNAME}:{DATABASE_PASSWORD}@{DATABASE_URL}:{DATABASE_PORT}"
