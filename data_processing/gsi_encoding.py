09eb978e6c09df01d837acfa787db329b5e24add
mseppi <miikka.seppi123@gmail.com>
2024-11-26 15:52:15 +0200

    add more data to be showed

--- data_processing/gsi_encoding.py
+++ data_processing/gsi_encoding.py
@@ -7,12 +7,15 @@ class DataEncoding:
 
         position_data = {}
         for steam_id, player_data in data["player_data"].items():
-            position_data[steam_id] = player_data["position"],player_data["name"], player_data["team"], player_data["health"]
+            position_data[steam_id] = player_data["position"],player_data["name"], player_data["team"], player_data["health"], player_data["forward"]
         self.write_json_file(position_data, "player_positions.json")
 
         statistics = {}
         for steam_id, player_data in data["player_data"].items():
-            statistics[steam_id] = player_data["name"], player_data["team"], player_data["health"], player_data["kills"], player_data["assists"], player_data["deaths"]
+            statistics[steam_id] = player_data["name"], player_data["team"], player_data["health"],
+            player_data["kills"], player_data["assists"], player_data["deaths"]
+        statistics["map"] = data["match_data"]["map"]
+        statistics["round"] = data["match_data"]["round"]
         self.write_json_file(statistics, "statistics.json")
     
     def write_json_file(self, data: dict, file_name: str):
